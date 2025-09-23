7ffbc2634c8126ad6a350533fc2fc2dd6a512586
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-09-23 16:31:16 +0200

    Add other login methods

--- src/db.rs
+++ src/db.rs
@@ -1,31 +1,31 @@
 use tokio_postgres::{Client, NoTls};
 
 use crate::{
-	types::{company::Company, student::Student, university::University},
-	utils::hash_password,
+    types::{company::Company, student::Student, university::University},
+    utils::hash_password,
 };
 
 async fn setup_database() -> Result<Client, String> {
-	let database_url =
-		std::env::var("DATABASE_URL").map_err(|_| "DATABASE_URL missing".to_string())?;
-	let (client, connection) = tokio_postgres::connect(&database_url, NoTls)
-		.await
-		.map_err(|e| format!("Connection failed: {e}"))?;
+    let database_url =
+        std::env::var("DATABASE_URL").map_err(|_| "DATABASE_URL missing".to_string())?;
+    let (client, connection) = tokio_postgres::connect(&database_url, NoTls)
+        .await
+        .map_err(|e| format!("Connection failed: {e}"))?;
 
-	tokio::spawn(async move {
-		if let Err(e) = connection.await {
-			eprintln!("Connection error: {e}");
-		}
-	});
-	Ok(client)
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Connection error: {e}");
+        }
+    });
+    Ok(client)
 }
 
 #[allow(clippy::missing_errors_doc)]
-pub async fn insert_universite(universite: University, twofa: String) -> Result<String, String> {
-	let client = setup_database().await?;
-	let password_hash = hash_password(&universite.password)?;
+pub async fn insert_university(universite: University, twofa: String) -> Result<String, String> {
+    let client = setup_database().await?;
+    let password_hash = hash_password(&universite.password)?;
 
-	let row = client
+    let row = client
         .query_one(
             "INSERT INTO university (name, mail, login, twofa, password) VALUES ($1, $2, $3, $4, $5) RETURNING id;",
             &[&universite.name, &universite.mail, &universite.login, &twofa, &password_hash],
@@ -33,21 +33,21 @@ pub async fn insert_universite(universite: University, twofa: String) -> Result<
         .await
         .map_err(|e| format!("INSERT error: {e}"))?;
 
-	let new_id: i32 = row.get(0);
-	println!("University created with id = {new_id}");
+    let new_id: i32 = row.get(0);
+    println!("University created with id = {new_id}");
 
-	Ok(format!(
-		"Values {}, {}, {}, {twofa}, {password_hash} (encoded password) inserted with id {new_id}",
-		universite.name, universite.mail, universite.login
-	))
+    Ok(format!(
+        "Values {}, {}, {}, {twofa}, {password_hash} (encoded password) inserted with id {new_id}",
+        universite.name, universite.mail, universite.login
+    ))
 }
 
 #[allow(clippy::missing_errors_doc)]
 pub async fn insert_student(student: Student, twofa: String) -> Result<String, String> {
-	let client = setup_database().await?;
-	let password_hash = hash_password(&student.password)?;
+    let client = setup_database().await?;
+    let password_hash = hash_password(&student.password)?;
 
-	let row = client
+    let row = client
         .query_one(
             "INSERT INTO student (first_name, last_name, login, twofa_secret, password, mail) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;",
             &[&student.first_name, &student.last_name, &student.login, &twofa, &password_hash, &student.mail],
@@ -55,21 +55,21 @@ pub async fn insert_student(student: Student, twofa: String) -> Result<String, S
         .await
         .map_err(|e| format!("INSERT Error: {e}"))?;
 
-	let new_id: i32 = row.get(0);
-	println!("Student created with id = {new_id}");
+    let new_id: i32 = row.get(0);
+    println!("Student created with id = {new_id}");
 
-	Ok(format!(
-		"Values {}, {}, {}, {}, {twofa}, {password_hash} (encoded password) inserted with id {new_id}",
-		student.login, student.first_name, student.last_name, student.mail
-	))
+    Ok(format!(
+        "Values {}, {}, {}, {}, {twofa}, {password_hash} (encoded password) inserted with id {new_id}",
+        student.login, student.first_name, student.last_name, student.mail
+    ))
 }
 
 #[allow(clippy::missing_errors_doc)]
 pub async fn insert_company(company: Company, twofa: String) -> Result<String, String> {
-	let client = setup_database().await?;
-	let password_hash = hash_password(&company.password)?;
+    let client = setup_database().await?;
+    let password_hash = hash_password(&company.password)?;
 
-	let row = client
+    let row = client
         .query_one(
             "INSERT INTO company (name, login, password, twofa, mail) VALUES ($1, $2, $3, $4, $5) RETURNING id;",
             &[&company.name, &company.login, &password_hash, &twofa, &company.mail],
@@ -77,25 +77,53 @@ pub async fn insert_company(company: Company, twofa: String) -> Result<String, S
         .await
         .map_err(|e| format!("INSERT error: {e}"))?;
 
-	let new_id: i32 = row.get(0);
-	println!("Company created with id = {new_id}");
+    let new_id: i32 = row.get(0);
+    println!("Company created with id = {new_id}");
 
-	Ok(format!(
-		"Values {}, {}, {}, {twofa}, {password_hash} (encoded password) inserted with id {new_id}",
-		company.login, company.name, company.mail
-	))
+    Ok(format!(
+        "Values {}, {}, {}, {twofa}, {password_hash} (encoded password) inserted with id {new_id}",
+        company.login, company.name, company.mail
+    ))
 }
 
 #[allow(clippy::missing_errors_doc)]
 pub async fn get_university_password_from_mail(mail: &str) -> Result<String, String> {
-	let client = setup_database().await?;
+    let client = setup_database().await?;
 
-	let row = client
-		.query_one("SELECT password FROM university WHERE mail=$1;", &[&mail])
-		.await
-		.map_err(|e| format!("SELECT error: {e}"))?;
+    let row = client
+        .query_one("SELECT password FROM university WHERE mail=$1;", &[&mail])
+        .await
+        .map_err(|e| format!("SELECT error: {e}"))?;
 
-	let hashed_password: String = row.get(0);
+    let hashed_password: String = row.get(0);
 
-	Ok(hashed_password)
+    Ok(hashed_password)
+}
+
+#[allow(clippy::missing_errors_doc)]
+pub async fn get_company_password_from_mail(mail: &str) -> Result<String, String> {
+    let client = setup_database().await?;
+
+    let row = client
+        .query_one("SELECT password FROM company WHERE mail=$1;", &[&mail])
+        .await
+        .map_err(|e| format!("SELECT error: {e}"))?;
+
+    let hashed_password: String = row.get(0);
+
+    Ok(hashed_password)
+}
+
+#[allow(clippy::missing_errors_doc)]
+pub async fn get_student_password_from_mail(mail: &str) -> Result<String, String> {
+    let client = setup_database().await?;
+
+    let row = client
+        .query_one("SELECT password FROM student WHERE mail=$1;", &[&mail])
+        .await
+        .map_err(|e| format!("SELECT error: {e}"))?;
+
+    let hashed_password: String = row.get(0);
+
+    Ok(hashed_password)
 }
--- src/main.rs
+++ src/main.rs
@@ -13,33 +13,33 @@ extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
-	match dotenvy::dotenv() {
-		Ok(_) => (),
-		Err(e) => {
-			eprintln!("Error while loading .env : {e}");
-			exit(1)
-		}
-	}
+    match dotenvy::dotenv() {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("Error while loading .env : {e}");
+            exit(1)
+        }
+    }
 
-	let rocket_secret = env::var("ROCKET_SECRET").ok().map_or_else(
-		|| {
-			eprintln!("Secret must be in .env");
-			exit(1)
-		},
-		|secret| secret,
-	);
+    let rocket_secret = env::var("ROCKET_SECRET").ok().map_or_else(
+        || {
+            eprintln!("Secret must be in .env");
+            exit(1)
+        },
+        |secret| secret,
+    );
 
-	let rocket = rocket::custom(Config::from(
-		Config::figment().merge(("secret_key", rocket_secret)),
-	));
+    let rocket = rocket::custom(Config::from(
+        Config::figment().merge(("secret_key", rocket_secret)),
+    ));
 
-	rocket.mount(
-		"/",
-		routes![
-			user::create_university,
-			user::create_student,
-			user::create_company,
-			login::login_university
-		],
-	)
+    rocket.mount(
+        "/",
+        routes![
+            user::create_university,
+            user::create_student,
+            user::create_company,
+            login::login_company
+        ],
+    )
 }
