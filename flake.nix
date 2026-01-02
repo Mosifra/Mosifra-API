2596bdcb3301e5cd7c940b0053522e85c627ae71
Agahnim <gabrielguysoulard@gmail.com>
2026-01-02 01:32:03 +0100

    finished flake

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = with pkgs; [cargo rustc rustfmt clippy rust-analyser];
+      buildInputs = with pkgs; [cargo rustc rustfmt clippy rust-analyzer];
       env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     };
   };
