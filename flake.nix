27f15e0ab9f0837f1bcd2f408ee99377701da7ea
Agahnim <gabrielguysoulard@gmail.com>
2026-01-27 11:29:29 +0100

    refacto to use tmux

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
-  description = "A very basic flake";
-
+  description = "Rust dev flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
-
   outputs = {
     self,
     nixpkgs,
@@ -19,10 +17,22 @@
         clippy
         rust-analyzer
         sqls
+        tmux
       ];
       env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
       shellHook = ''
-        neovide &
+        SESSION_NAME="rust-dev"
+
+        if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
+          tmux new-session -d -s $SESSION_NAME -n "editor"
+          tmux send-keys -t $SESSION_NAME:0 "neovide" C-m
+
+          tmux new-window -t $SESSION_NAME:1 -n "shell"
+
+          tmux select-window -t $SESSION_NAME:1
+        fi
+
+        tmux attach-session -t $SESSION_NAME
       '';
     };
   };
