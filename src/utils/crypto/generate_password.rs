903c7ffde4e48b6cb4e1222455621954fb48b746
Nissyaniss <nissya@proton.me>
2026-02-12 15:00:52 +0100

    fixed passwords

--- src/utils/crypto/generate_password.rs
+++ src/utils/crypto/generate_password.rs
@@ -1,19 +1,28 @@
-use passwords::PasswordGenerator;
+use rand::prelude::IndexedRandom;
+use rand::seq::SliceRandom;
 use rocket::http::Status;
-
-use crate::error_handling::StatusResultHandling;
-
+ 
 #[allow(clippy::missing_errors_doc)]
 pub fn generate_password() -> Result<String, Status> {
-	PasswordGenerator::new()
-		.length(8)
-		.numbers(true)
-		.lowercase_letters(true)
-		.uppercase_letters(true)
-		.symbols(true)
-		.spaces(false)
-		.exclude_similar_characters(true)
-		.strict(true)
-		.generate_one()
-		.internal_server_error("Error while generating password")
+	const CHARSET_UPPER: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const CHARSET_LOWER: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+	const CHARSET_NUMBER: &[u8] = b"0123456789";
+	const CHARSET_SYMBOL: &[u8] = b"!/?";
+
+	let mut rng = rand::rng();
+	let mut password_chars = Vec::with_capacity(8);
+
+	password_chars.push(*CHARSET_UPPER.choose(&mut rng).unwrap() as char);
+	password_chars.push(*CHARSET_LOWER.choose(&mut rng).unwrap() as char);
+	password_chars.push(*CHARSET_NUMBER.choose(&mut rng).unwrap() as char);
+	password_chars.push(*CHARSET_SYMBOL.choose(&mut rng).unwrap() as char);
+
+	let all_chars = [CHARSET_UPPER, CHARSET_LOWER, CHARSET_NUMBER, CHARSET_SYMBOL].concat();
+	for _ in 0..4 {
+		password_chars.push(*all_chars.choose(&mut rng).unwrap() as char);
+	}
+
+	password_chars.shuffle(&mut rng);
+
+	Ok(password_chars.into_iter().collect())
 }
